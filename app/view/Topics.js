8273eb572a8f6a124936dfdbc3b2f07a5eaed1dd
prasannamishra <pmishra@innominds.com>
2013-08-19 12:54:21 +0530

    Profile details is added to Topics page

--- app/view/Topics.js
+++ app/view/Topics.js
@@ -5,7 +5,7 @@ Ext.define('Perzo.view.Topics',{
 	config:{
 		styleHtmlContent:true,
 		layout:{
-			type:'vbox'
+			type:'fit'
 		},
 		
           
@@ -34,7 +34,44 @@ Ext.define('Perzo.view.Topics',{
 						}
 					]
           	},{
-          		xtype:'spacer'
+          		xtype:'container',
+          		layout:'vbox',
+          		align:'stretch',
+          		scrollable:true,
+          		items:[{
+          				xtype:'container',
+						flex:1,
+          				layout:'hbox',
+          				cls:'topics-profile',
+          				style:'background: #363636;',
+          				items:[{
+          						flex:3,
+          						tpl:'<div class="topics-profile-container">'+
+          									'<div><img src="" /></div>'+
+          									'<div class="topics-profile-lastActivity">Last active 6.20PM</div>'+
+          									'<div class="topics-profile-metadata">'+
+          									'<div class="topic-profile-status">I Love Perzo !</div>'+
+          									'<div class="topic-profile-weather-status"></div>'+
+          									'<div class="topic-profile-location">Palo Alto, CA</div>'+
+          									'<div class="topic-profile-temperature">11.54 PM, 39<sup>0</sup>C</div>'+
+          									'</div>'+
+	          						'</div>',
+          						data:{name:'kali'}
+          					},
+          					  { 
+          					  	flex:2,
+          					  	
+          					  	tpl:'<div class="topic-profile-single-div"></div><div class="topic-profile-single-div"></div>',
+          						data:{noOfDivs:2}
+          					}
+          					]
+          				},
+          				{
+          					xtype:'container',
+          					flex:1
+          					
+          				}
+          		]
           	},{
           		 xtype : 'toolbar',
             	 docked: 'bottom',
